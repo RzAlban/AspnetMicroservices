853e94cf0b42a2e02672baff2b02fe39a0c2a4d8
Alban Qoku <aqoku@revzonesolutions.com>
2022-11-11 10:17:09 +0100

    Catalog.Api and Basket.Api completed

---
+++ src/Services/Basket/Baske.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.Entities;
+using System.Threading.Tasks;
+
+namespace Baske.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
